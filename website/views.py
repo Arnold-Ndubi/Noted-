aab6b6601d5d8ace910e9c739a7829c5f49708fd
Arnold-Ndubi <ndubyarnold95@gmail.com>
2024-08-11 12:16:09 +0300

    creating routes

--- website/views.py
+++ website/views.py
@@ -27,11 +27,4 @@ def home():
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
+    noteId =)
