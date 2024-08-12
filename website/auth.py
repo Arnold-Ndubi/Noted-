5611fe25e0696f8c654c1f3bd98ebe5d95f09cea
Arnold-Ndubi <ndubyarnold95@gmail.com>
2024-08-12 05:32:58 +0300

    defining routes

--- website/auth.py
+++ website/auth.py
@@ -58,9 +58,4 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(
                 password1, method='sha256'))
             db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user, remember=True)
-            flash('Account created!', category='success')
-            return redirect(url_for('views.home'))
-
-    return render_template("sign_up.html", user=current_user)
+            db.session.co)
