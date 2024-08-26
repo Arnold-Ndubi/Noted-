2e52ef594edeb0e20cdf782bcbfdf99f430fce21
Arnold-Ndubi <ndubyarnold95@gmail.com>
2024-08-26 04:54:58 +0300

    Adding user sign-up with validation, password hashing, and feedback.

--- website/auth.py
+++ website/auth.py
@@ -60,5 +60,4 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
-            flash('Account created!', category='success')
-            
+            flash('Account created!', c 
