85f19b786928721a5456c5174af75021cdd329fb
Arnold-Ndubi <ndubyarnold95@gmail.com>
2024-08-27 21:13:24 +0300

    debuging the signup.html file

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db   ##means from __init__.py import db
+from . import db   # means from __init__.py import db
 from flask_login import login_user, login_required, logout_user, current_user
 
 
@@ -55,12 +55,15 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
+            new_user = User(
+                email=email, 
+                first_name=first_name, 
+                password=generate_password_hash(password1, method='pbkdf2:sha256')
+            )
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html", user=current_user) 
+    return render_template("sign_up.html", user=current_user)
